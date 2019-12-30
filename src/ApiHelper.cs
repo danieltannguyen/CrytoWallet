c3c958718e80dc234ac4f318106e0a51e4aca20d
codeskiller <danieltn91@gmail.com>
2019-12-29 23:17:13 -0800

    added web coinmarketcap api and .net.http framwork

---
+++ src/ApiHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Formatting;
+using System.Net.Http;
+
+namespace CrytoWallet.src
+{
+    class ApiHelper
+    {
+        HttpClient httpC = new HttpClient();
+        httpC.BaseAddress = new Uri("https://pro-api.coinmarketcap.com/v1/cryptocurrency/map");
+        HttpResonseMessage response = client.GetAsync("").Result;
+        var price = response.Content.ReadAsAsync<IEnumerable<Price>>().Result;
+        label.Text = price;
+    }
+}
